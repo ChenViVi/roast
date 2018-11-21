92da306557755abd7f2ddd343d13a5498abebe04
chenvivi <932942491@qq.com>
2018-11-21 16:38:34 +0800

    opps，又忘记import

--- app/Http/Controllers/Web/AuthenticationController.php
+++ app/Http/Controllers/Web/AuthenticationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use App\User;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
 
 class AuthenticationController extends Controller
